8f4cb79bce3ea272d92487500691f6f6d1b25c8a
yasmincapar <y.capar@yahoo.com>
2023-05-11 14:16:44 +0100

    initial commit

---
+++ src/main/java/com/ecommerceshopping/ecommerceshopping/EcommerceshoppingApplication.java
@@ -0,0 +1,13 @@
+package com.ecommerceshopping.ecommerceshopping;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerceshoppingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommerceshoppingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ecommerceshopping/ecommerceshopping/EcommerceshoppingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecommerceshopping.ecommerceshopping;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceshoppingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
