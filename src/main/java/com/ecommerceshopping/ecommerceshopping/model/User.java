67874672fca063c1f0ddcf66fd11317a694d20c1
yasmincapar <y.capar@yahoo.com>
2023-05-11 16:14:37 +0100

    In this commit I have added Product and Category classes with attributes in them.

--- src/main/java/com/ecommerceshopping/ecommerceshopping/model/User.java
+++ src/main/java/com/ecommerceshopping/ecommerceshopping/model/User.java
@@ -1,4 +1,26 @@
 package com.ecommerceshopping.ecommerceshopping.model;
 
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.context.annotation.Scope;
+
+@Data
+@Builder
+@Entity
+@Table(name="user_table")
+@Scope("session")
+@AllArgsConstructor
+@NoArgsConstructor
 public class User {
+    @Id
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    private int id;
+    private String email;
+    private String username;
+    private String password;
+    private String confirmPassword;
 }
